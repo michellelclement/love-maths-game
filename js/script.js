44cf3f5687b297afa1f12aed0c56708270bb8dd0
Michelle Clement <michellelclement@gmail.com>
2021-01-06 11:34:55 +0000

    Checking user answer to correct answer in place

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
-})
+});
 
 
 function runGame(gameType) {
@@ -37,6 +37,21 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+
+    // Checks the answer against the first element in
+    // the returned calculatedCorrectAnswer array
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D")
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
